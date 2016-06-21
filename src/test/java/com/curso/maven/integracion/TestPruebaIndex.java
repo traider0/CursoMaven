647f7a00b93aab44c9494fea655a690137f5d781
Victor <victorherrerocazurro@gmail.com>
2016-06-21 19:54:38 +0200

    Adaptacion para el curso de La Rozas

--- src/test/java/com/curso/maven/integracion/TestPruebaIndex.java
+++ src/test/java/com/curso/maven/integracion/TestPruebaIndex.java
@@ -24,7 +24,7 @@ public class TestPruebaIndex {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "http://localhost:8082/";
+    baseUrl = "http://localhost:8080/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
